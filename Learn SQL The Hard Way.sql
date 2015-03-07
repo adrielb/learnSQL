3e697d59ddc51f51d851212185a424e39df38593
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-03-07 15:05:15 -0500

    11: Replacing data

--- Learn SQL The Hard Way.sql
+++ Learn SQL The Hard Way.sql
@@ -254,4 +254,20 @@ WHERE id IN (
 SELECT * FROM pet;
 
 
+/* 11: Replacing Data */
+/* update without transactions */
+INSERT INTO person (id, first_name, last_name, age) VALUES
+        ( 0, 'Frank', 'Smith', 100);
 
+INSERT OR REPLACE INTO person (id, first_name, last_name, age) VALUES
+        ( 0, 'Frank', 'Smith', 100);
+
+SELECT * FROM person;
+
+REPLACE INTO person (id, first_name, last_name, age) VALUES
+        ( 0,  'Zed', 'Shaw', 37);
+
+SELECT * FROM pet;
+
+REPLACE INTO pet (id, name, breed, age, dead) VALUES
+        (0, 'peeky', "Parrot", 10, 0);
